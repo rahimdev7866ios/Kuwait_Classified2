dee5176da4afaba0153e3c0104835cfd14de45e7
rahimBasha <rahimdev786@gmail.com>
2021-09-14 16:03:28 +0530

    Initial Commit

---
+++ Kuwait_Classified2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kuwait_Classified2
+//
+//  Created by shaik Arshad on 14/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
